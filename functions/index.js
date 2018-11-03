5b39137b8b801b341d6a571633a66c99ae9b38c8
plin0009 <plin0009@gmail.com>
2018-11-03 03:47:23 -0400

    Firebase start

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
+const engines = require('consolidate');
+
+const firebaseApp = firebase.initializeApp(functions.config().firebase);
+
+function getData() {
+  const ref = firebaseApp.database().ref('data');
+  return ref.once('value').then(snap => snap.val());
+}
+const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  getData().then(data => {
+    res.render('index', {data});
+  });
+});
+exports.app = functions.https.onRequest(app);
