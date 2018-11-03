f106393148f350cc7d594f7dc2ab97b80988ba9f
plin0009 <plin0009@gmail.com>
2018-11-03 15:54:14 -0400

    Trying to get authentication working

--- functions/index.js
+++ functions/index.js
@@ -16,33 +16,37 @@ app.use(express.static('views'));
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
-
+console.log('test');
 passport.use(new FitbitStrategy({
   clientID: '22D59S',
   clientSecret: '593fa72b8cacede4644a48c0ff53f8dd',
   callbackURL: 'https://echacks-892cd.firebaseapp.com/auth/callback/'
 }, (accessToken, refreshToken, profile, done) => {
-  setUser({id: profile.id}, done);
+  setUser({id: profile.id}, (err, user) => {
+    return done(err, user)
+  });
 }));
 app.get('/', (req, res) => {
   res.render('index', {code:"Homepage"});
 });
-app.get('/auth', passport.authenticate('fitbit', {scope: ['heartrate', 'activity']}));
+app.get('/auth', passport.authenticate('fitbit'));
 app.get('/auth/callback', passport.authenticate('fitbit', {
   failureRedirect: 'auth/failure'
-}), (req, res) => {
-  res.redirect("/");
-});
+}, (req, res) => {
+  res.redirect('/success');
+}));
 
 exports.app = functions.https.onRequest(app);
 
 function setUser(data, callback) {
+  console.log(data, callback);
   usersRef.child(data.id).transaction((currentData) => {
     if (currentData === null) {
       return data;
     }
     return usersRef.child(data.id);
   }, (error, committed, snapshot) => {
+    console.log(error, committed, snapshot);
     callback(error, snapshot);
   });
 }
