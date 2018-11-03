0ba42885a29946212dc29959a942754b6f014c97
plin0009 <plin0009@gmail.com>
2018-11-03 19:27:27 -0400

    Working on callback page

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,13 @@ const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
 const express = require('express');
 const engines = require('consolidate');
+const FitbitApiClient = require('fitbit-node');
 
 const firebaseApp = firebase.initializeApp(functions.config().firebase);
 const database = firebase.database();
 
 const app = express();
-
+const client = new FitbitApiClient({clientId: '22D59S', clientSecret: '593fa72b8cacede4644a48c0ff53f8dd'});
 app.use(express.static('views'));
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
@@ -16,34 +17,12 @@ app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
   res.render('index', {code:"Homepage"});
 });
-app.get('/auth', (req, res) => {});
-app.get('/auth/callback', (req, res) => {});
+app.get('/auth', (req, res) => {
+  let url = 'https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22D59S&redirect_uri=https%3A%2F%2Fechacks-892cd.firebaseapp.com%2Fcallback&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800';
+  res.redirect(url);
+});
+app.get('/callback', (req, res) => {
+  res.render('callback');
+});
 
 exports.app = functions.https.onRequest(app);
-<<<<<<< HEAD
-=======
-
-function setUser(data, callback) {
-  console.log(data, callback);
-  usersRef.child(data.id).transaction((currentData) => {
-    if (currentData === null) {
-      return data;
-    }
-    return usersRef.child(data.id);
-  }, (error, committed, snapshot) => {
-    console.log(error, committed, snapshot);
-    callback(error, snapshot);
-  });
-}
-
-
-function getInfo(userID) {
-  let url = `https://api.fitbit.com/1/user/${userID}/activities/heart/date/today/1d.json`;
-  request(url, function (err, response, body) {
-    let information = JSON.parse(body);
-    if (information.activities-heart) {
-      res.render('index', {code: information.activities-heart[0].restingHeartRate});
-    }
-  });
-}
->>>>>>> 20501de0e78e41e82ffd3bd4d66fdfff8ae4d6d3
