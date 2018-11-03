a9bf65ddb9aed176975e6a0a6ce1f0431d895bf5
Alan Wang <alan.wang9018@gmail.com>
2018-11-03 18:18:42 -0400

    Edited graph and info layout

--- functions/views/css/app.js
+++ functions/views/css/app.js
@@ -1,20 +1,19 @@
 let chart=document.getElementById('graph1').getContext('2d');
 
 let myChart =new Chart(chart, {
-    type:'line',
+    type:'bar',
     data:{
-        labels:[1,2,3,4,5,6,7],
+        labels:['Mon','Tues','Wed','Thurs','Fri','Sat','Sun'],
         datasets:[{
             data:[141,235,412,535,698,729,834],
-            fill:false,
-            borderColor:"#e32d2d",
-            label:"line"
+            backgroundColor:"rgba(8, 18, 42, 0.842)",
+            label:"Calories Burned"
         }],
     },
     options:{
         title:{
             display:true,
-            text:"TITLE"
+            text:"Daily Activity"
         },
         layout:{
 
