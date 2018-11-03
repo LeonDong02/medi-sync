683464d7d96527661c92b12558817b7f5aa373e6
plin0009 <plin0009@gmail.com>
2018-11-03 12:00:36 -0400

    Trying to make OAuth work

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ const firebase = require('firebase-admin');
 const express = require('express');
 const engines = require('consolidate');
 const request = require('request');
+const http = require('http');
+const querystring = require('querystring');
 
 const firebaseApp = firebase.initializeApp(functions.config().firebase);
 
@@ -20,8 +22,25 @@ app.get('/auth', (req, res) => {
   let code = req.query.code;
   if (code) {
     res.render('index', {code});
+    request.post({
+      headers: {
+        'Authorization': 'Basic MjJENTlTOjU5M2ZhNzJiOGNhY2VkZTQ2NDRhNDhjMGZmNTNmOGRk',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      url: 'https://api.fitbit.com/oauth2/token',
+      form: {
+        'clientId': '22D59S',
+        'grant-type': 'authorization-code',
+        'redirect-uri': 'https%3A%2F%2Fechacks-892cd.firebaseapp.com%2Ftoken%2F',
+        'code': code
+      }
+    }, (error, response, body) => {
+      console.log(response, body);
+    });
   }
 });
-
+app.get('/token', (req, res) => {
+  res.render('index', {req.query.token});
+});
 
 exports.app = functions.https.onRequest(app);
