b77c32d7f0d51d2fbe2f8f0678c53afc5ce14240
Alan Wang <alan.wang9018@gmail.com>
2018-11-03 11:53:23 -0400

    Added Chartjs Graph

---
+++ functions/views/css/app.js
@@ -0,0 +1,26 @@
+let chart=document.getElementById('graph1').getContext('2d');
+
+let myChart =new Chart(chart, {
+    type:'line',
+    data:{
+        labels:[1,2,3,4,5,6,7],
+        datasets:[{
+            data:[141,235,412,535,698,729,834],
+            fill:false,
+            borderColor:"blue",
+            label:"line"
+        }],
+    },
+    options:{
+        title:{
+            display:true,
+            text:"TITLE"
+        },
+        layout:{
+
+        },
+        responsive:false,
+        maintainAspectRatio:true
+    }
+
+})
