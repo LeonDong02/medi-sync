8175a4013c9eb5786d77aa489db96dc159811afd
plin0009 <plin0009@gmail.com>
2018-11-04 09:28:47 -0500

    f

--- functions/index.js
+++ functions/index.js
@@ -36,7 +36,9 @@ app.get('/dashboard/*', (req, res) => {
   let userId = req.url.substring(11);
   let ref = database.ref('users/' + userId);
   ref.on('value', snapshot => {
-    res.render('index', {data:snapshot.val()});
+    let data = snapshot.val();
+    data.sleep.summary.totalMinutesAsleep /= 60;
+    res.render('index', {data});
   });
 });
 
