6f8deadd8940bd10329b63554b072b963ae466e5
plin0009 <plin0009@gmail.com>
2018-11-04 06:06:57 -0500

    Moved GET request to client side

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const engines = require('consolidate');
 const bodyParser = require('body-parser');
 const FitbitApiClient = require('fitbit-node');
+const https = require('https');
 
 var access_token, user_id, scope, token_type, expires_in;
 
@@ -21,7 +22,7 @@ app.set('views', './views');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-  res.render('index', {code:"Homepage"});
+  res.render('landing');
   // render a signup page instead?
 });
 app.get('/auth', (req, res) => {
@@ -29,11 +30,7 @@ app.get('/auth', (req, res) => {
   res.redirect(url);
 });
 app.get('/callback', (req, res) => {
-  if (access_token) {
-    res.render('index', {code: 'it works'});
-  } else {
-    res.render('callback');
-  }
+  res.render('callback');
 });
 app.get('/landing', (req, res) => {
   // req.query.access_token
@@ -47,7 +44,15 @@ app.get('/landing', (req, res) => {
   scope = req.query.scope;
   token_type = req.query.token_type;
   expires_in = req.query.expires_in;
-  res.render('callback');
+  res.send("landing");
+  https.get('https://api.fitbit.com/1/user/-/profile.json', {
+    headers: {
+      'Authorization': 'Bearer ' + req.query.access_token
+    }
+  }, r => {
+    res.send(r);
+  });
+  // res.redirect('callback');
 });
 
 exports.app = functions.https.onRequest(app);
