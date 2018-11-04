b00190eed1d6d268df099fe2f1c5d70ed641c8ed
plin0009 <plin0009@gmail.com>
2018-11-04 06:45:12 -0500

    Retreiving more data from Fitbit

--- functions/views/js/fitbit.js
+++ functions/views/js/fitbit.js
@@ -1,14 +1,31 @@
 var vars = {};
+var data = {};
 window.onload = function() {
   //window.location.replace('https://echacks-892cd.firebaseapp.com/landing?' + decodeURI(window.location.hash.substring(1)));
   let hash = decodeURI(window.location.hash);
   hash.replace(/[#&]+([^=&]+)=?([^&]*)?/gi, (m, key, value) => {
     vars[key] = value;
   });
+  let d = new Date(),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
 
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  let today = [year, month, day].join('-');
+  // get data
+  getData(today).then(console.log(data));
+}
+async function getData(today) {
+  data.profile = await fetchData('profile');
+  data.heart = await fetchData('activities/heart/date/today/1d');
+  data.bodyFat = await fetchData('body/log/fat/date/' + today + '/1d');
+  // more
 }
 
-function getData(url) {
+function fetchData(url) {
   return fetch('https://api.fitbit.com/1/user/-/' + url + '.json', {
     'method': 'GET',
     'headers': {
